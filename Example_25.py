5ae3f3a0cd727e9cb6f375fec3bf155a4fde9d16
liufei <fei.liu@qyer.com>
2017-10-24 23:36:04 +0800

    Save

---
+++ Example_25.py
@@ -0,0 +1,28 @@
+# coding: utf-8
+'''
+题目：求1+2!+3!+...+20!的和。
+'''
+def jiechengFn(x):
+    if x == 1:
+        return 1
+    else:
+        return x*jiechengFn(x-1)
+
+def sumFn(x):
+    sum = 0L
+    for i in range(1, x+1):
+        sum += jiechengFn(i)
+    return sum
+
+
+print sumFn(20)
+
+#=======================================#
+def Fn(x):
+    a = 1
+    s = 0L
+    for i in range(1, x+1):
+        a *= i
+        s += a
+    return s
+print Fn(20)
