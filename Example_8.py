8a88747c8596c26e01ea95e09c06b575f1605b38
fei.liu <fei.liu@qyer.com>
2019-04-27 22:10:26 +0800

    Default

--- Example_8.py
+++ Example_8.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 # 题目：输出 9*9 乘法口诀表。
 
-for i in range(1, 10):
+for i in range(1,10):
     for j in range(1, i+1):
-        print i, "x", j, "=", i*j, " ",
+        print "%d * %d = %d" % (i, j, i*j),
     print
