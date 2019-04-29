e2448a4d13bafbc89cd0c32b6350d8a65ed6c4e2
fei.liu <fei.liu@qyer.com>
2019-04-29 13:44:47 +0800

    Save

--- Example_00.py
+++ Example_00.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 class Solution(object):
     def __init__(self):
-        self.r, self.t = 1, 1
+        self.r = 1
     def lengthOfLongestSubstring(self, s):
         """
         :type s: str
@@ -12,15 +12,15 @@ class Solution(object):
             return 0
         for i in range(l-1):
             rl = [s[i]]
+            self.t = 1
             for j in range(i+1, l):
                 rl.append(s[j])
                 if len(rl) == len(list(set(rl))):
                     self.t +=1
                     if self.t > self.r:
                         self.r = self.t
-                        print "[%s, %s]" % (i, j), self.t, self.r, rl
-            else:
-                self.t = 1
+                else:
+                    break
         return self.r
 
-print Solution().lengthOfLongestSubstring("aaaaaaaaaaaabbbbbbbbbbbbbbbbbbcccccccccccccdasdfaea4rq34yw46io6798pu;i")
+print Solution().lengthOfLongestSubstring("pwwkew")
