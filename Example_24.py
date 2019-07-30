88010115406fc06aed2467e163daaab5660b9d06
fei.liu <fei.liu@qyer.com>
2019-07-30 10:53:57 +0800

    Save

--- Example_24.py
+++ Example_24.py
@@ -3,13 +3,16 @@
 题目：有一分数序列：2/1，3/2，5/3，8/5，13/8，21/13...求出这个数列的前20项之和。
 '''
 
-def fn(x):
-    a, b = 2.0, 1.0
-    sum = 0
-    for i in range(x):
-        b, a = a, a + b
-        sum += a/b
-    return sum
+def testFunc():
+    a, b = 1.0, 1.0
+    result = 0.0
 
-print fn(20)
+    for i in range(20):
+        a, b = a+b, a
+        result += a/b
+    return result
+
+if __name__ == "__main__":
+    print testFunc()
+
 
