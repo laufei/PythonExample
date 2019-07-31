70ab49abac4c001852a40f28cb05d25ddcf65735
fei.liu <fei.liu@qyer.com>
2019-07-31 18:38:09 +0800

    Save

---
+++ 最长连续递增序列.py
@@ -0,0 +1,24 @@
+# coding: utf-8
+
+# @Time    : 2019/7/31 6:02 PM
+# @Author  : 'liufei'
+# @Email   : fei.liu@qyer.com
+# @File    : 最长连续递增序列.py
+# @Software: PyCharm
+
+def testFun(list):
+    list_len = len(list)
+    if list_len < 2:
+        return list_len
+    count, tmp = 1, 1
+    for i in range(list_len-1):
+        if list[i] < list[i+1]:
+            tmp += 1
+            if tmp > count:
+                count = tmp
+        else:
+            tmp = 1
+    return count
+
+if __name__ == "__main__":
+    print testFun([21,22,23,1,2,3,4,6,5])
