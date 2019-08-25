027c878b55df6ab8f82e1094d4ac463aca004267
fei.liu <fei.liu@qyer.com>
2019-08-25 12:54:43 +0800

    Save

--- 检查IP有效性.py
+++ 检查IP有效性.py
@@ -5,20 +5,31 @@
 # @Email   : fei.liu@qyer.com
 # @File    : 检查IP有效性.py
 # @Software: PyCharm
+import re
 
 def testFunc(ip):
-    ips_list = ip.split(".")
-    ips_len = len(ips_list)
-    if ips_len < 4:
-        return False
-    for i in ips_list:
-        try:
-            i = int(i)
-        except:
-            return False
-        result = 256 > i > 0
-    return result
+    ip_list= ip.split(".")
+    ip_list_len = len(ip_list)
+    if 4 != ip_list_len:
+        return "非法IP"
+    for i in ip_list:
+        if not (re.match(r"\d{1,3}$", i) and int(i) < 255 and int(i) >=0) :
+            return "非法IP"
+        else:
+            return "合法IP"
 
 if __name__ == "__main__":
-    ip = "10.1.1"
-    print testFunc(ip)
+    ip1 = "10.1.1"
+    ip2 = "asdfasdf"
+    ip3 = "a.b.c.d"
+    ip4 = "10.0."
+    ip5 = "10.1.1.1.1"
+    ip6 = "10a.1.1.i"
+    ip7 = "10.1.1.10i"
+    print testFunc(ip1)
+    print testFunc(ip2)
+    print testFunc(ip3)
+    print testFunc(ip4)
+    print testFunc(ip5)
+    print testFunc(ip6)
+    print testFunc(ip7)
