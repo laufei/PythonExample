52c1730125600d3362d7fe92a4336f7e838c0691
fei.liu <fei.liu@qyer.com>
2019-08-25 16:10:30 +0800

    Save

--- 最长连续递增序列.py
+++ 最长连续递增序列.py
@@ -10,14 +10,16 @@ def testFun(list):
     list_len = len(list)
     if list_len < 2:
         return list_len
+    begin = 0
     count, tmp = 1, 1
     for i in range(list_len-1):
         if list[i] < list[i+1]:
             tmp += 1
+            begin = i - count + 1
             count = max(tmp, count)
         else:
             tmp = 1
-    return count
+    return list[begin: begin + count]
 
 if __name__ == "__main__":
     print testFun([21,22,23,1,2,3,4,6,5])
