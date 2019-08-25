0f2c516931ba2bcc2da3581dd7d396ff5a76f933
fei.liu <fei.liu@qyer.com>
2019-08-25 15:06:00 +0800

    Save

--- 最长不重复字符串.py
+++ 最长不重复字符串.py
@@ -7,7 +7,24 @@
 # @Software: PyCharm
 
 def testFun(str):
-
+    str_len = len(str)
+    if str_len < 2:
+        return str
+    index_begin = 0
+    index_end = 1
+    for i in range(str_len-1):
+        tmp = 1
+        tmp_list = [str[i]]
+        for j in range(i+1, str_len):
+            tmp_list.append(str[j])
+            if all([str[i] != str[j], len(set(tmp_list)) == len(tmp_list)]):
+                tmp += 1
+            else:
+                break
+        if tmp > index_end:
+            index_begin = i
+            index_end = tmp
+    return str[index_begin: index_begin+index_end]
 
 
 if __name__ == "__main__":
