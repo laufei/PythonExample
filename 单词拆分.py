189f613d3fc5472a8e907bcbc7d26fea81a3dac7
fei.liu <fei.liu@qyer.com>
2019-08-27 12:51:21 +0800

    Save

--- 单词拆分.py
+++ 单词拆分.py
@@ -6,32 +6,30 @@
 # @File    : 单词拆分.py
 # @Software: PyCharm
 
-# def wordBreak(s, wordDict):
-#     """
-#     :type s: str
-#     :type wordDict: List[str]
-#     :rtype: bool
-#     """
-#     if not s:
-#         return True
-#
-#     breakp = [0]
-#
-#     for i in range(len(s) + 1):
-#         for j in breakp:
-#             if s[j:i] in wordDict:
-#                 breakp.append(i)
-#                 break
-#     return breakp[-1] == len(s)
-
 def wordBreak(s, wordDict):
+    """
+    :type s: str
+    :type wordDict: List[str]
+    :rtype: bool
+    """
     if not wordDict:
         return False
 
-    max_sub_len = max([len(x) for x in wordDict])
-
     breakp = [0]
 
+    for i in range(len(s) + 1):
+        for j in breakp:
+            if s[j:i] in wordDict:
+                breakp.append(i)
+                break
+    return breakp[-1] == len(s)
+
+'''
+def wordBreak(s, wordDict):
+    if not wordDict:
+        return False
+    max_sub_len = max([len(x) for x in wordDict])
+    breakp = [0]
     for i in range(len(s) + 1):
         if i - breakp[0] > max_sub_len:
             breakp.pop(0)
@@ -44,7 +42,7 @@ def wordBreak(s, wordDict):
                 break
 
     return breakp[-1] == len(s)
-
+'''
 s = "leetcode"
 wordDict = ["leet", "code"]
 print wordBreak(s, wordDict)
