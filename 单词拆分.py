934c4d40636a8effed78cec310bfd1bee7070485
fei.liu <fei.liu@qyer.com>
2019-08-27 12:36:16 +0800

    Save

--- 单词拆分.py
+++ 单词拆分.py
@@ -7,16 +7,24 @@
 # @Software: PyCharm
 
 def wordBreak(s, wordDict):
-    if not s:
-        return True
+    if not wordDict:
+        return False
+
+    max_sub_len = max([len(x) for x in wordDict])
 
     breakp = [0]
 
     for i in range(len(s) + 1):
+        if i - breakp[0] > max_sub_len:
+            breakp.pop(0)
+            if not breakp:
+                return False
+
         for j in breakp:
             if s[j:i] in wordDict:
                 breakp.append(i)
                 break
+
     return breakp[-1] == len(s)
 
 s = "leetcode"
