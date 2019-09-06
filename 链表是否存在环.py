075a62bc9f2cfa1f6ee49be1566b6908f2f85d3d
fei.liu <fei.liu@qyer.com>
2019-09-06 17:22:17 +0800

    --

--- 链表是否存在环.py
+++ 链表是否存在环.py
@@ -1,2 +1,25 @@
 # coding: utf-8
+class Node(object):
+    def __init__(self, data, next=None):
+        self.val = data
+        self.next = next
 
+def hasCycle(head):
+	fast = slow = head
+	while slow and fast and fast.next:
+		slow = slow.next
+		fast = fast.next.next
+		if slow is fast:
+			return True
+	return False
+
+# # 测试用例
+if __name__ == '__main__':
+    l1 = Node(3)
+    l1.next = Node(2)
+    l1.next.next = Node(1)
+    l1.next.next.next = Node(5)
+    l1.next.next.next.next = Node(9)
+    l1.next.next.next.next.next = l1.next.next
+    print (l1.val, l1.next.val, l1.next.next.val, l1.next.next.next.val, l1.next.next.next.next.val, l1.next.next.next.next.next.val)
+    print hasCycle(l1)
--- 链表相邻节点反转.py
+++ 链表相邻节点反转.py
@@ -2,19 +2,26 @@
 
 class Node(object):
     def __init__(self, data, next=None):
-        self._val = data
-        self._next = next
+        self.val = data
+        self.next = next
 
-def reverseList(head):
-    pass
+class test(object):
+	def swapPairs(self, head):
+		pre, pre.next = self, head
+		while pre.next and pre.next.next:
+			a = pre.next
+			b = a.next
+			pre.next, b.next, a.next = b, a, b.next
+			pre = a
+		return self.next
 
 # # 测试用例
 if __name__ == '__main__':
     l1 = Node(3)
-    l1._next = Node(2)
-    l1._next._next = Node(1)
-    l1._next._next._next = Node(5)
-    l1._next._next._next._next = Node(9)
-    print (l1._val, l1._next._val, l1._next._next._val, l1._next._next._next._val)
-    l = reverseList(l1)
-    print (l._val, l._next._val, l._next._next._val, l._next._next._next._val)
+    l1.next = Node(2)
+    l1.next.next = Node(1)
+    l1.next.next.next = Node(5)
+    l1.next.next.next.next = Node(9)
+    print (l1.val, l1.next.val, l1.next.next.val, l1.next.next.next.val, l1.next.next.next.next.val)
+    l = test().swapPairs(l1)
+    print (l.val, l.next.val, l.next.next.val, l.next.next.next.val, l.next.next.next.next.val)
