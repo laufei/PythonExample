5e59fb987a0c05b5ff61abac3ac5c3213b490592
fei.liu <fei.liu@qyer.com>
2019-09-06 16:50:47 +0800

    --

--- 有效的字母异位词.py
+++ 有效的字母异位词.py
@@ -1,1 +1,13 @@
 # coding: utf-8
+
+def isAnagram(s, t):
+	dic1, dic2 = {}, {}
+	for item in s:
+		dic1[item] = dic1.get(item, 0) + 1
+	for item in t:
+		dic2[item] = dic2.get(item, 0) + 1
+	return dic1 == dic2
+
+s = "artew"
+t = "ertaw"
+print isAnagram(s, t)
