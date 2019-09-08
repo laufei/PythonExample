7cbd90cf80b4c03df2f426297a7ea867bcd908db
fei.liu <fei.liu@qyer.com>
2019-09-08 12:10:18 +0800

    --

--- 0.找到字符串中所有字母异位词.py
+++ 0.找到字符串中所有字母异位词.py
@@ -36,10 +36,10 @@ def findAnagrams(s, p):
 		smap[v] = smap.get(v, 0) + 1
 		print(v, smap)
 		if smap == pmap:
-			rlist.append(i-plenth+1)
-		if i - plenth + 1 >= 0:     # 如果遍历的长度>=p的长度
+			rlist.append(i-plenth+1)                                               # 加一是因为索引从0开始的
+		if i - plenth + 1 >= 0:                                                        # 如果遍历的长度>=p的长度
 			smap[s[i-plenth + 1]] = smap.get(s[i-plenth + 1]) - 1   # 在遍历下个元素v只前, 将最左元素的个数-1
-			if smap[s[i-plenth + 1]] == 0:                                      # 如果计数为0, 则删掉, 否则影响2个dict的对比
+			if smap[s[i-plenth + 1]] == 0:                                       # 如果计数为0, 则删掉, 否则影响2个dict的对比
 				del smap[s[i-plenth + 1]]
 		print(v, smap)
 	return rlist
