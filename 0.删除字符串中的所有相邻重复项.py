0594836e028682a1a5a09759964f2e000dbe4f5c
fei.liu <fei.liu@qyer.com>
2019-09-09 12:22:56 +0800

    --

--- 0.删除字符串中的所有相邻重复项.py
+++ 0.删除字符串中的所有相邻重复项.py
@@ -1,16 +1,12 @@
 # coding: utf-8
 
 def removeDuplicates(S):
-    """
-    :type S: str
-    :rtype: str
-    """
     # 初始化栈
     stack = []
     # 遍历栈元素
     for e in S:
         if stack and stack[-1] == e:
-            stack.pop()
+            stack.pop(-1)
         else:
             stack.append(e)
     return "".join(stack)
