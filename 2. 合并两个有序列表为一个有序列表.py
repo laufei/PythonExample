087628647ef7a2726a134a3d201b2c1f077238f6
fei.liu <fei.liu@qyer.com>
2019-09-10 13:20:19 +0800

    --

--- 2. 合并两个有序列表为一个有序列表.py
+++ 2. 合并两个有序列表为一个有序列表.py
@@ -14,10 +14,11 @@ def merge(l1, l2):
 		while i1 < len(l1) and i2 < len(l2):
 			if l1[i1] < l2[i2]:
 				l.append(l1[i1])
-				i1+= 1
+				i1 += 1
 			else:
 				l.append(l2[i2])
 				i2 += 1
+		print l
 		if i1 < len(l1):
 			l.extend(l1[i1:])
 		if i2 < len(l2):
@@ -27,4 +28,4 @@ def merge(l1, l2):
 		return None
 	return l
 
-print(merge([1, 3, 3, 5, 6], [2, 4, 4, 6, 7]))
+print(merge([1, 3, 3, 5, 6, 7, 7, 7, 7, 7, 7, 7, 7], [2, 4, 4, 6, 7, 7, 7, 7, 7]))
