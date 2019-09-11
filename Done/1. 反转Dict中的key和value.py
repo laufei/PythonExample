456006b6858d69b56272ee2e8350875acefa37bb
fei.liu <fei.liu@qyer.com>
2019-09-11 15:57:37 +0800

    --

--- Done/1. 反转Dict中的key和value.py
+++ Done/1. 反转Dict中的key和value.py
@@ -8,6 +8,7 @@
 
 d = {'a': 1, 'b': 2, 'c': 3}
 def revert_dict(source):
-    return dict((v, k) for k, v in d.items())
+    return {v:k for k, v in d.items()}
+    # return dict((v, k) for k, v in d.items())
 if __name__ == "__main__":
     print revert_dict(d)
