9dd5d7e64bf4273e5abcc45da207ff7cd68832a2
fei.liu <fei.liu@qyer.com>
2019-09-11 16:36:12 +0800

    --

--- 1. 最长连续序列.py
+++ 1. 最长连续序列.py
@@ -7,26 +7,22 @@
 # @Software: PyCharm
 
 class Solution(object):
-    def longestConsecutive(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        tmp, count = 1, 1
-        nums = sorted(list(set(nums)))
-        nLen = len(nums)
-        if nLen < 2:
-            return nLen
-        for i in range(nLen-1):
-            if nums[i+1] == 1 + nums[i]:
-                tmp += 1
-                if tmp > count:
-                    count = tmp
-            else:
-                tmp = 1
-        return count
+	def longestConsecutive(self, nums):
+		begin, tmp, count = 0, 1, 1
+		nums = sorted(list(set(nums)))
+		nLen = len(nums)
+		if nLen < 2:
+			return nLen
+		for i in range(nLen-1):
+			if nums[i+1] == 1 + nums[i]:
+				tmp += 1
+				count = max(tmp, count)
+			else:
+				tmp = 1
+			print begin, count
+		return nums[begin: begin + count]
 
 if __name__ == "__main__":
-    l = [21,22,23,1,2,3,4,6,5]
-    s = Solution()
-    print (s.longestConsecutive(l))
+	l = [21,22,23,1,2,3,4,6,5]
+	s = Solution()
+	print (s.longestConsecutive(l))
