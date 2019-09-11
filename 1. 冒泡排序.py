d8ebe3bd797b06be86ac3471fd54ea3aeb45a129
fei.liu <fei.liu@qyer.com>
2019-09-11 16:00:32 +0800

    --

--- 1. 冒泡排序.py
+++ 1. 冒泡排序.py
@@ -7,7 +7,7 @@ def bubbleSort(arr):
         for j in range(n-i-1):
             if arr[j] > arr[j+1]:
                 arr[j], arr[j+1] = arr[j+1], arr[j]
-
+    return arr
 arr = [64, 34, 25, 12, 22, 11, 90]
 
 print(bubbleSort(arr))
