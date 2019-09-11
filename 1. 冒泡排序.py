8312d24f656a56d26efefac5095ed6ea81527ae5
fei.liu <fei.liu@qyer.com>
2019-09-11 16:09:18 +0800

    --

--- 1. 冒泡排序.py
+++ 1. 冒泡排序.py
@@ -1,14 +1,15 @@
 # coding: utf-8
 
 def bubbleSort(arr):
-    n = len(arr)
+	n = len(arr)
 
-    for i in range(n):
-        for j in range(n-i-1):
-            if arr[j] > arr[j+1]:
-                arr[j], arr[j+1] = arr[j+1], arr[j]
-    return arr
-arr = [64, 34, 25, 12, 22, 11, 90]
+	for i in range(n):
+		for j in range(n-i-1):
+			print arr
+			if arr[j] > arr[j+1]:
+				arr[j], arr[j+1] = arr[j+1], arr[j]
+	return arr
+arr = [64, 34, 25, 12, 22, 11, 90, 20]
 
 print(bubbleSort(arr))
 
