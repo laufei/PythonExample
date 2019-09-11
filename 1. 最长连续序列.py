6adc5a4a376b0653b1a7c7a3edf65800d43d4380
fei.liu <fei.liu@qyer.com>
2019-09-11 17:01:26 +0800

    --

--- 1. 最长连续序列.py
+++ 1. 最长连续序列.py
@@ -1,28 +1,21 @@
 # coding: utf-8
 
-# @Time    : 2019/7/31 10:28 PM
-# @Author  : 'liufei'
-# @Email   : fei.liu@qyer.com
-# @File    : 最长连续序列.py
-# @Software: PyCharm
 
-class Solution(object):
-	def longestConsecutive(self, nums):
-		begin, tmp, count = 0, 1, 1
-		nums = sorted(list(set(nums)))
-		nLen = len(nums)
-		if nLen < 2:
-			return nLen
-		for i in range(nLen-1):
-			if nums[i+1] == 1 + nums[i]:
-				tmp += 1
-				count = max(tmp, count)
-			else:
-				tmp = 1
-			print begin, count
-		return nums[begin: begin + count]
+def longestConsecutive(nums):
+	if len(nums) < 2:
+		return len(nums)
+	begin = 0
+	count = 1
+	maxc = 1
+	for i in range(1, len(nums)):
+		if nums[i - 1] + 1 == nums[i]:
+			count += 1
+			maxc = max(count, maxc)
+			begin = i - maxc + 1
+		else:
+			count = 1
+	return nums[begin: begin + maxc]
 
 if __name__ == "__main__":
 	l = [21,22,23,1,2,3,4,6,5]
-	s = Solution()
-	print (s.longestConsecutive(l))
+	print (longestConsecutive(l))
--- 1. 最长连续递增序列.py
+++ 1. 最长连续递增序列.py
@@ -6,20 +6,20 @@
 # @File    : 最长连续递增序列.py
 # @Software: PyCharm
 
-def testFun(list):
-    list_len = len(list)
-    if list_len < 2:
-        return list_len
+def findLengthOfLCIS(nums):
+    if len(nums) < 2:
+        return len(nums)
     begin = 0
-    count, tmp = 1, 1
-    for i in range(list_len-1):
-        if list[i] < list[i+1]:
-            tmp += 1
-            begin = i - count + 1
-            count = max(tmp, count)
+    count = 1
+    maxc = 1
+    for i in range(1, len(nums)):
+        if nums[i] > nums[i - 1]:
+            count += 1
+            maxc = max(count, maxc)
+            begin = i -  maxc + 1
         else:
-            tmp = 1
-    return list[begin: begin + count]
+            count = 1
+    return nums[begin: begin + maxc]
 
 if __name__ == "__main__":
-    print testFun([21,22,23,1,2,3,4,6,5])
+    print findLengthOfLCIS([21,22,23,1,2,3,4,6,5])
