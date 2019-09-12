10e6d94ba9a8e461dc418ac566b89bcc98d74fe0
fei.liu <fei.liu@qyer.com>
2019-09-12 08:04:40 +0800

    --

--- Doing/3. 滑动窗口中的最大值.py
+++ Doing/3. 滑动窗口中的最大值.py
@@ -4,12 +4,13 @@ def maxSlidingWindow(nums, k):
 	if not nums: return []
 	window, res = [], []
 	for i, x in enumerate(nums):
-		if i >= k and window[0] <= i - k:
+		print "i= %d, k= %d, windows= %s, res= %s" % (i, k, window, res)
+		if i >= k and window[0] <= i - k:                       # 删除掉window范围左侧的值
 			window.pop(0)
-		while window and nums[window[-1]] <= x:
-			window.pop()
+		while window and nums[window[-1]] <= x:     # window中的值小于刚刚进入窗口的值
+			window.pop(-1)
 		window.append(i)
-		if i >= k - 1:
+		if i >= k - 1:                                                      # 遍历的位置>=窗口大小
 			res.append(nums[window[0]])
 	return res
 
