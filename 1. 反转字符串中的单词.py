f9290b625fa6816a858f03a3376d2fa88bd4508b
fei.liu <fei.liu@qyer.com>
2019-09-14 14:33:35 +0800

    --

--- 1. 反转字符串中的单词.py
+++ 1. 反转字符串中的单词.py
@@ -1,14 +1,7 @@
 # coding: utf-8
 
-# @Time    : 2019/8/27 9:20 AM
-# @Author  : 'liufei'
-# @Email   : fei.liu@qyer.com
-# @File    : 反转字符串中的单词.py
-# @Software: PyCharm
-
 def reverStr(string):
     return " ".join([i[::-1] for i in string.split(" ")])
-
 
 if __name__ == "__main__":
     string = "Let's take LeetCode contest"
