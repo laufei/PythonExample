75531c05371312ccabf33b2e54bb05d022f4b8a3
fei.liu <fei.liu@qyer.com>
2019-09-14 14:46:52 +0800

    --

--- Doing/1. 递归计算阶乘.py
+++ Doing/1. 递归计算阶乘.py
@@ -1,17 +1,9 @@
 # coding: utf-8
 
-# @Time    : 2019/7/30 4:25 PM
-# @Author  : 'liufei'
-# @Email   : fei.liu@qyer.com
-# @File    : 递归计算阶乘.py
-# @Software: PyCharm
-
-def testFunc(input):
-    if input == 1:
+def testFunc(n):
+    if n == 1:
         return 1
     else:
-        result = input * testFunc(input-1)
-    return result
-
+        return n * testFunc(n - 1)
 if __name__ == "__main__":
     print testFunc(5)
