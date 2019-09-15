1912c11d9a4dd2a18a65926bfe1d7a855539c98b
fei.liu <fei.liu@qyer.com>
2019-09-15 18:53:54 +0800

    --

--- Doing/1. 搜索插入位置.py
+++ Doing/1. 搜索插入位置.py
@@ -14,9 +14,9 @@ def searchInsert(nums, target):
     left = 0
     right = len(nums)
     while left < right:
-        mid = left + (right - left) // 2
+        mid = (left + right) // 2
         if nums[mid] > target:
-            right = mid
+            right = mid - 1
         elif nums[mid] < target:
             left = mid + 1
         else:
