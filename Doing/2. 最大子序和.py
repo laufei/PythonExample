f21f8e0a913ae1d908c9e89b8a3344b160564f7b
fei.liu <fei.liu@qyer.com>
2019-09-16 16:53:11 +0800

    --

--- Doing/2. 最大子序和.py
+++ Doing/2. 最大子序和.py
@@ -16,3 +16,20 @@ def maxSubArray(nums):
     return max(nums)
 
 print(maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4]))
+
+def getMaxList(nums):
+    nlen = len(nums)
+    nmax = 0
+    begin = 0
+    end = 0
+    for i in range(nlen - 1):
+        tmp = nums[i]
+        for j in range(i+1, nlen):
+            tmp += nums[j]
+            if nmax < tmp:
+                nmax = tmp
+                begin = i
+                end = j
+    return nums[begin: end + 1]
+nums = [-4, 5, 8, -9, 12, -4, 0, 1]
+print(getMaxList(nums))
